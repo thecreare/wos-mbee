c85ee470889d403d0fcb4b0802146a22dfabfd48
= <makerbenjammin@gmail.com>
2025-01-01 12:54:48 -0500

    Improve overwrite tool

--- tools/Overwrite.lua
+++ tools/Overwrite.lua
@@ -1,3 +1,24 @@
+--[[
+This tool is used to generate a new parts folder that is a composition of both MB and MBEE
+
+First it creates a set of every part name between both folders
+Then merges them together based on the following rules
+- Parts in the `EXCLUDE` list are always taken from MBEE
+- Parts that only exist in MBEE are taken from MBEE
+- Parts that only exist in MB are taken from MB
+- Parts that exist in both MB and MBEE are taken from MB
+]]
+
+-- Parts that exist in both but have been otherwise modified by MBEE
+local EXCLUDE = {
+	"GravityGenerator", -- Gravity [1 > 0.15]
+
+	-- Changed size from 6x1x1 to 8x1x1 to match other logistic cable types.
+	"Pipe",
+	"Chute",
+	"HeatPipe",
+}
+
 local Selection = game:GetService("Selection")
 
 function DeselectAll()
@@ -21,12 +42,16 @@ local baseline = AwaitClick()
 print(`Please select MBEE parts folder`)
 local customs = AwaitClick()
 
+local OUTPUT_FOLDER = Instance.new("Folder")
+OUTPUT_FOLDER.Name = `SyncedPartsFolder_{os.time()}`
+OUTPUT_FOLDER.Parent = workspace
+
 local all_parts = {}
 
 for _, v in baseline:GetChildren() do
 	if not table.find(all_parts, v.Name) then
 		table.insert(all_parts, v.Name)
-	end	
+	end
 end
 
 for _, v in customs:GetChildren() do
@@ -37,25 +62,30 @@ end
 
 print(all_parts)
 
-local to_steal_from_custom = {}
+for _, part_name in all_parts do
+	local baseline_part = baseline:FindFirstChild(part_name) -- MB
+	local custom_part = customs:FindFirstChild(part_name) -- MBEE
 
-for _, part in all_parts do
-	local baseline_part = baseline:FindFirstChild(part)
-	local custom_part = customs:FindFirstChild(part)
-	
-	if baseline_part and not custom_part then
-		print(`MB has part {part} and MBEE does not`)
+	local part
+
+	if table.find(EXCLUDE, part_name) then
+		-- If part has been modified and should not be overwrote by MB's parts
+		part = custom_part:Clone()
+	elseif baseline_part and not custom_part then
+		-- Part exists in MB but doesn't exist in MBEE
+		-- Aka new part was added to wos
+		part = baseline_part:Clone()
+	elseif custom_part and not baseline_part then
+		-- Part exists in MBEE but doesn't exist in MB
+		-- Aka developer only part thats not listed in MB or a part was removed from wos
+		part = custom_part:Clone()
+	elseif custom_part and baseline_part then
+		-- Part exists in both MBEE and MB
+		-- Take from MB (for updated config reasons)
+		part = baseline_part:Clone()
+	else
+		error("This shouldn't happen")
 	end
-	
-	if custom_part and not baseline_part then
-		print(`MBEE has part {part} and MB does not`)
-	end
-	
-	if not baseline_part and custom_part then
-		table.insert(to_steal_from_custom, custom_part)
-	end
+
+	part.Parent = OUTPUT_FOLDER
 end
-
-
-Selection:Set(to_steal_from_custom)
-print(`There are {#to_steal_from_custom} parts that only exist in mbee. They have automatically been selected.`)
