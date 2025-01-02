ec846dec79fe2cdb14b1f4232ee22339fe3a39a0
= <makerbenjammin@gmail.com>
2025-01-02 16:18:18 -0500

    "Improve" logger

--- src/Modules/Logger.lua
+++ src/Modules/Logger.lua
@@ -1,8 +1,26 @@
+local WARN_TRACEBACK = false
+local PREFIX_BASE = "MBEE"
+
+local function GetPrefix()
+	return `[{PREFIX_BASE}]:`
+end
+
 local module = {}
+
 function module.print(...)
-	warn("[MBEE]:", ...)
+	warn(GetPrefix(), ...)
 end
+
 function module.warn(...)
-	warn("[MBEE]:", ..., debug.traceback())
+	if WARN_TRACEBACK then
+		warn(GetPrefix(), ..., debug.traceback())
+	else
+		warn(GetPrefix(), ...)
+	end
 end
+
+function module.error(...)
+	warn(GetPrefix(), ..., debug.traceback())
+end
+
 return module
