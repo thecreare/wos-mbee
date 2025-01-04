894df80c2be99c5ba04137e8549de152b9b5cbd9
= <makerbenjammin@gmail.com>
2025-01-03 20:54:07 -0500

    Improve patcher and make child mesh detection more resilient

--- src/Compilers/2.6.1/PartMetadata/init.lua
+++ src/Compilers/2.6.1/PartMetadata/init.lua
@@ -1,7 +1,8 @@
---[[
+--[[PB]]--[[
 This file was automatically modified by tools/compiler_patcher.py
+2025-01-03
 ]]
-
+--[[PE]]
 
 --!strict
 local HttpService = game:GetService("HttpService")
@@ -107,8 +108,16 @@ function PartMetadata:GetShape(part: BasePart): string?
 	local mesh = part:FindFirstChildWhichIsA("SpecialMesh")
 	if mesh and mesh.MeshType == Enum.MeshType.Sphere then
 		return "Spheroid"
-	--[[PB]]elseif mesh and shapesByMeshId[mesh.MeshId] then
+	--[[PB]]elseif mesh and mesh.MeshType == Enum.MeshType.Brick then
+		return "nil"
+	--[[PE]]--[[PB]]elseif mesh and mesh.MeshType == Enum.MeshType.Wedge then
+		return "Wedge"
+	--[[PE]]--[[PB]]elseif mesh and mesh.MeshType == Enum.MeshType.Cylinder then
+		return "Cylinder"
+	--[[PE]]--[[PB]]elseif mesh and shapesByMeshId[mesh.MeshId] then
 		return shapesByMeshId[mesh.MeshId]
+	--[[PE]]--[[PB]]elseif mesh and mesh.MeshId == "http://www.roblox.com/asset/?id=11294911" then
+		return "CornerWedge"
 	--[[PE]]elseif part:IsA("Part") then
 		local shape = part.Shape
 
--- tools/compiler_patcher.py
+++ tools/compiler_patcher.py
@@ -1,10 +1,16 @@
-import time
+"""
+Is this cursed? Yes.
+Is it better than copy-pasting changes for every new compiler? Also yes.
+"""
+import re
+import datetime
 
 COMPILER = "2.6.1"
 FILE_PATH = f"./src/Compilers/{COMPILER}"
 
-FILE_PATCH_HEADER = """--[[
+FILE_PATCH_HEADER = f"""--[[
 This file was automatically modified by tools/compiler_patcher.py
+{datetime.date.today()}
 ]]
 """
 
@@ -13,6 +19,7 @@ PATCH_END = "--[[PE]]"
 
 opened_files = {}
 
+MATCH_PATCHES = re.compile(r"--\[\[PB\]\].*?--\[\[PE\]\]", re.S)
 def GetContents(path):
     if path in opened_files:
         return opened_files[path]
@@ -20,13 +27,19 @@ def GetContents(path):
     # Get contents
     with open(path, "r") as f:
         contents = f.read()
+
+    # Remove old patches
+    contents = re.sub(MATCH_PATCHES, "", contents)
+
+    # Remember the contents for future open attempts and for when contents are written back at the end
     opened_files[path] = contents
     return contents
 
+# For every opened file write its contents back to its file path.
+# Add a header as well
 def WritebackFiles():
     for path, contents in opened_files.items():        
-        if contents.find(FILE_PATCH_HEADER) == -1:
-            contents = FILE_PATCH_HEADER + "\n\n" + contents
+        contents = PATCH_BEGIN + FILE_PATCH_HEADER + PATCH_END + contents
 
         with open(path, "w") as f:
             f.write(contents)
@@ -47,10 +60,27 @@ def PrependPatchTo(path, after, find):
 
 # Patch :GetShape() to support base parts with `SpecialMesh`'s
 # Request: https://discord.com/channels/616089055532417036/1047587493693886547/1324649555526025278
+PART_METADATA = "PartMetadata/init.lua"
+GET_SHAPE_ROOT = 'elseif part:IsA("Part") then'
+# Things that use MeshType
+for mesh, return_value in {"Brick": "nil", "Wedge": "Wedge", "Cylinder": "Cylinder"}.items():
+    PrependPatchTo(
+        PART_METADATA,
+        f'elseif mesh and mesh.MeshType == Enum.MeshType.{mesh} then\n\t\treturn "{return_value}"\n\t',
+        GET_SHAPE_ROOT,
+    )
+    
+# Things that use MeshId
 PrependPatchTo(
-    "PartMetadata/init.lua",
+    PART_METADATA,
     'elseif mesh and shapesByMeshId[mesh.MeshId] then\n\t\treturn shapesByMeshId[mesh.MeshId]\n\t',
-    'elseif part:IsA("Part") then',
+    GET_SHAPE_ROOT,
+)
+# Edge case for CornerWedge
+PrependPatchTo(
+    PART_METADATA,
+    'elseif mesh and mesh.MeshId == "http://www.roblox.com/asset/?id=11294911" then\n\t\treturn "CornerWedge"\n\t',
+    GET_SHAPE_ROOT,
 )
 
 WritebackFiles()
