9fd024f07323f3d60b5027942d24bb9243027fbb
creare <makerbenjammin@gmail.com>
2025-07-17 02:28:23 -0400

    Move part list ui to module

--- src/init.client.lua
+++ src/init.client.lua
@@ -48,7 +48,6 @@ local PseudoInstance = require(script.MBEPackages.PseudoInstance)
 
 local LuaEncode = require(script.MBEPackages.LuaEncode)
 local repr = require(script.MBEPackages.repr)
-local Constants = require(script.Modules.Constants)
 
 local CustomModules = script.Modules
 local Components = script.Components
@@ -640,217 +639,20 @@ UserInputService[table.concat({"Win","dowFoc","used"}, "")]:Connect(function()
 	if part3 then part3:Destroy() end
 end)
 
-local BG = Instance.new("ScrollingFrame")
-BG.Size = UDim2.new(1, 0, 1, 0)
-BG.CanvasSize = UDim2.new(1, 0, 1, 0)
-BG.AutomaticCanvasSize = Enum.AutomaticSize.Y
-BG.ScrollBarThickness = 0
-BG.ScrollingDirection = Enum.ScrollingDirection.Y
-table.insert(UIElements.Frames, BG)
+local BG = scope:ScrollingFrame {
+	ListPadding = UDim.new(0, 10),
+	ScrollBarThickness = 0,
+	[Children] = {
+		require(script.PartList),
+	}
+} :: ScrollingFrame
 
-local BGUIList = Instance.new("UIListLayout")
-BGUIList.SortOrder = Enum.SortOrder.LayoutOrder
-BGUIList.FillDirection = Enum.FillDirection.Vertical
-BGUIList.HorizontalAlignment = Enum.HorizontalAlignment.Center
-BGUIList.Padding = UDim.new(0, 10)
-BGUIList.Parent = BG
-
-local SearchBoxHolder = Instance.new("Frame")
-SearchBoxHolder.Size = UDim2.new(1, -6, 0, PrimaryWidget.AbsoluteSize.Y - 248) -- UDim2.new(1, -6, 0, 30)
-SearchBoxHolder.LayoutOrder = 0
-SearchBoxHolder.Parent = BG
-
-PrimaryWidget.Changed:Connect(function(Property)
-	if Property ~= "AbsoluteSize" then return end
-	TweenService:Create(SearchBoxHolder, TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), { Size = UDim2.new(1, 0, 0, PrimaryWidget.AbsoluteSize.Y - 248) } ):Play()
-	plugin:SetSetting("PluginSize", {{PrimaryWidget.AbsoluteSize.X, PrimaryWidget.AbsoluteSize.Y}, {ConfigWidget.AbsoluteSize.X, ConfigWidget.AbsoluteSize.Y}})
-end)
-
-local SearchBox = Instance.new("TextBox")
-SearchBox.BorderSizePixel = 1
-SearchBox.Size = UDim2.new(1, 0, 0, 16)
-SearchBox.Text = ''
-SearchBox.PlaceholderText = 'Search [Item/Category]'
-SearchBox.Font = Enum.Font.SourceSansLight
-SearchBox.TextXAlignment = Enum.TextXAlignment.Left
-SearchBox.TextScaled = true
-SearchBox.Parent = SearchBoxHolder
-table.insert(UIElements.Boxes, SearchBox)
-
-local SearchMatches = Instance.new("TextLabel")
-SearchMatches.BorderSizePixel = 1
-SearchMatches.TextTransparency = 0.25
-SearchMatches.BackgroundTransparency = 1
-SearchMatches.Size = UDim2.new(1, 0, 0, 16)
-SearchMatches.Text = ''
-SearchMatches.Font = Enum.Font.SourceSansLight
-SearchMatches.TextXAlignment = Enum.TextXAlignment.Left
-SearchMatches.TextScaled = true
-SearchMatches.Visible = false
-SearchMatches.Active = false
-SearchMatches.Parent = SearchBoxHolder
-table.insert(UIElements.ContrastLabels, SearchMatches)
-
-local FocusSearch = plugin:CreatePluginAction("MBEFocusSearchQuery", "[MB:E:E] Focus Search", "Focuses on the search bar.", "", true)
-FocusSearch.Triggered:Connect(function()
-	task.wait()
-	SearchBox:CaptureFocus()
-end)
-
---[[SearchBox:GetPropertyChangedSignal("Text"):Connect(function()
-	if SearchBox.Text == "" then OrganiseResults() return end
-	OrganiseResults(SearchBox.Text)
-end)]]
-UITemplates.ConnectBoxToAutocomplete(SearchBox, script.Parts:GetChildren()).Event:Connect(function(MatchedParts)
-	if InfoConstants.SearchCategories[SearchBox.Text:lower()] then
-		local CategoryItems = {}
-		for _, Part in script.Parts:GetChildren() do
-			for _, CategoryItem in InfoConstants.SearchCategories[SearchBox.Text:lower()] do
-				if Part.Name:lower() ~= CategoryItem:lower() then continue end
-				CategoryItems[Part.Name] = true
-			end
-		end
-
-		if SearchBox.Text:lower() == "templates" then
-			local TemplateMaterial = ExtractedUtil.MatchQueryToList(TemplateMaterial.Box.Text, script.Parts:GetChildren())[1]
-			if TemplateMaterial then
-				CategoryItems[tostring(TemplateMaterial)] = true
-			end
-		end
-
-		for _, SearchButton in ResultsFrame:GetChildren() do
-			if not SearchButton:IsA("GuiBase") then continue end
-			if not CategoryItems[SearchButton.Name] then
-				SearchButton.Visible = false
-				continue
-			end
-			SearchButton.Visible = true
-		end
-
-		return
-	end
-
-	--task.wait()
-
-	if SearchBox.Text == "" then
-		ListLayout.SortOrder = Enum.SortOrder.Name
-		for _, SearchButton in ResultsFrame:GetChildren() do
-			if not SearchButton:IsA("TextButton") then continue end
-			SearchButton.Visible = true
-		end
-		return
-	end
-
-	ListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	for _, SearchButton in ResultsFrame:GetChildren() do
-		if not SearchButton:IsA("TextButton") then continue end
-		for ListOrder, Part in MatchedParts do
-			if SearchButton.Name ~= tostring(Part) then
-				SearchButton.Visible = false
-				continue
-			end
-
-			SearchButton.LayoutOrder = ListOrder
-			SearchButton.Visible = true
-			break
-		end
-	end
-end)
-
-SearchBox.FocusLost:Connect(function(EnterPressed)
-	task.wait()
-
-	if not EnterPressed then return end
-
-	local MatchTo = SearchMatches.Visible and string.lower(SearchMatches.Text) or string.lower(SearchBox.Text)
-	local Part
-	for _, _Part in script.Parts:GetChildren() do
-		if string.lower(_Part.Name) ~= MatchTo then continue end
-		Part = _Part
-	end
-
-	if not Part then return end
-
-	ListLayout.SortOrder = Enum.SortOrder.Name
-	for i, SearchButton in ResultsFrame:GetChildren() do
-		if not SearchButton:IsA("TextButton") then continue end
-		SearchButton.Visible = true
-	end
-
-	SearchBox.Text, SearchMatches.Text, SearchMatches.Visible = "", "", false
-
-	ExtractedUtil.SpawnPart(Part)
-end)
-
-ResultsFrame = Instance.new("ScrollingFrame")
-ResultsFrame.Size = UDim2.new(1, 0, 1, -32)
-ResultsFrame.AnchorPoint = Vector2.new(0, 0)
-ResultsFrame.Position = UDim2.fromOffset(0, 16)
-ResultsFrame.BorderSizePixel = 1
-ResultsFrame.ScrollBarThickness = 6
-ResultsFrame.Parent = SearchBoxHolder
-table.insert(UIElements.Scrolls, ResultsFrame)
-
-ListLayout = Instance.new("UIListLayout", ResultsFrame)
-ListLayout.SortOrder = Enum.SortOrder.Name
-
-local AddMaterialButton = PseudoInstance.new("RippleButton")
-AddMaterialButton.PrimaryColor3 = Colors.MainContrast
-AddMaterialButton.Size = UDim2.new(1, 0, 0, 16)
-AddMaterialButton.AnchorPoint = Vector2.new(0.5, 1)
-AddMaterialButton.Position = UDim2.new(.5, 0, 1, 0)
-AddMaterialButton.BorderRadius = 2
-AddMaterialButton.Style = "Contained"
-AddMaterialButton.Text = "Add Material"
-AddMaterialButton.Font = Enum.Font.SourceSans
-AddMaterialButton.TextSize = 16
-AddMaterialButton.Parent = SearchBoxHolder
-table.insert(UIElements.Buttons, AddMaterialButton)
-
-AddMaterialButton.OnPressed:Connect(function()
-	if #Selection:Get() <= 0 then warn('[MB:E:E] SELECT A PART TO TURN INTO A MATERIAL') return end
-	if typeof(Selection:Get()[1]) ~= "Instance" then return end
-	local Material = Selection:Get()[1]
-	if not Material:IsA("BasePart") then return end
-
-	for _, Resource in pairs(InfoConstants.SearchCategories.resources) do
-		if Resource == Material.Name:lower() then
-			warn('[MB:E:E] ' .. Material.Name:upper() .. ' ALREADY EXISTS')
-			return
-		end
-	end
-
-	local NewMaterial = Material:Clone()
-	NewMaterial.Parent = script.Parts
-
-	CustomMaterialsModule.Add(NewMaterial.Name, {
-		Material = NewMaterial.Material.Name,
-		Transparency = NewMaterial.Transparency,
-		Reflectance = NewMaterial.Reflectance,
-		Color = {math.floor(NewMaterial.Color.R * 255), math.floor(NewMaterial.Color.G * 255), math.floor(NewMaterial.Color.B * 255)},
-		Size = {NewMaterial.Size.X, NewMaterial.Size.Y, NewMaterial.Size.Z}
+PrimaryWidget:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
+	plugin:SetSetting("PluginSize", {
+		{PrimaryWidget.AbsoluteSize.X, PrimaryWidget.AbsoluteSize.Y},
+		{ConfigWidget.AbsoluteSize.X, ConfigWidget.AbsoluteSize.Y}
 	})
-
-	UITemplates.CreateObjectButton({Part = NewMaterial, Deletable = true, Parent = ResultsFrame})
-	table.insert(InfoConstants.SearchCategories.resources, NewMaterial.Name:lower())
-	table.insert(InfoConstants.SearchCategories.templateables, NewMaterial.Name:lower())
-	warn('[MB:E:E] ' .. NewMaterial.Name:upper() .. ' WAS SUCCESSFULLY TURNED INTO A MATERIAL')
 end)
-
-TemplateMaterial = UITemplates.UITemplatesCreateTextBox(
-	{
-		Name = "TemplateMaterial",
-		LabelText = "Template Material",
-		BoxPlaceholderText = "Resource [string]",
-		Parent = BG,
-		LayoutOrder = 1,
-	})
-UITemplates.ConnectBoxToAutocomplete(TemplateMaterial.Box, script.Parts:GetChildren()).Event:Connect(function(Matched)
-	if #Matched > 16 then return end
-	if Matched[1] == nil then return end
-	ExtractedUtil.ApplyTemplates(Selection:Get(), Matched[1])
-end)
-ExtractedUtil.StupidGlobals.TemplateMaterial = TemplateMaterial
 
 -- MARK: Setting UI
 -- Create the UI buttons and stuff to control internal setting states
@@ -1488,59 +1290,7 @@ scope:Container {
 	}
 }
 
-
-do -- Create object buttons for normal wos parts
-	local found_map = {}
-	for _, Part in pairs(script.Parts:GetChildren()) do
-		UITemplates.CreateObjectButton({Part = Part, Parent = ResultsFrame})
-		found_map[Part.Name] = true
-	end
-
-	if Constants.IS_LOCAL then
-		for part_name, _ in CompilersModule:GetAllMalleability() do
-			if found_map[part_name] == nil then
-				Logger.warn(`Missing model for part {part_name}. Inserting placeholder.`)
-				local Part = Instance.new("Part")
-				Part.Color = BrickColor.Random().Color
-				Part.Size = Vector3.one*2
-				Part.Name = part_name
-				Part.Anchored = true
-				Part:AddTag("Placeholder")
-				Part.Parent = script.Parts
-				UITemplates.CreateObjectButton({Part = Part, Parent = ResultsFrame})
-			end
-		end
-	end
-end
-
--- Create part button for each custom material
-local MaterialsLoaded = pcall(function()
-	for Name, Properties in CustomMaterialsModule.CustomMaterials do
-		local NewMaterial = Instance.new("Part")
-		NewMaterial.Anchored = true
-		NewMaterial.Name = Name
-		for Property, PropertyValue in Properties do
-			if Property == "Color" then NewMaterial[Property] = Color3.fromRGB(PropertyValue[1], PropertyValue[2], PropertyValue[3]) continue end
-			if Property == "Size" then NewMaterial[Property] = Vector3.new(PropertyValue[1], PropertyValue[2], PropertyValue[3]) continue end
-			NewMaterial[Property] = PropertyValue
-		end
-		NewMaterial.Parent = script.Parts
-		table.insert(InfoConstants.SearchCategories.resources, Name:lower())
-		table.insert(InfoConstants.SearchCategories.templateables, Name:lower())
-		UITemplates.CreateObjectButton({Part = NewMaterial, Deletable = true, Parent = ResultsFrame})
-	end
-end)
-
-if not MaterialsLoaded then
-	CustomMaterialsModule.Clear()
-end
-
-ResultsFrame.CanvasSize = UDim2.fromScale(0, 0)
-ResultsFrame.AutomaticCanvasSize = Enum.AutomaticSize.Y
-
 BG.Parent = PrimaryWidget
-
-
 
 local CompilerSettings = {}
 CompilerSettings.CombinerName = "WoS Tools"
