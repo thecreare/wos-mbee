b8f88bfba5a6f03e9a66071229850d4aa9419950
creare <makerbenjammin@gmail.com>
2025-07-16 21:01:41 -0400

    Switch branding when in local dev environment

--- src/Modules/Branding.lua
+++ src/Modules/Branding.lua
@@ -1,4 +1,16 @@
-return {
-    NAME = "Model Builder: Edited Edited",
-    NAME_ABBREVIATION = "MBEE",
-}
+local plugin = _G.plugin
+local is_local = if string.find(plugin.Name, ".rbxm") or string.find(plugin.Name, ".lua") then true else false
+
+if is_local then
+    -- Developer environment branding
+    return {
+        NAME = "Model Builder: Edited Edited [DEV]",
+        NAME_ABBREVIATION = "MBEE_DEV",
+    }
+else
+    -- Production branding
+    return {
+        NAME = "Model Builder: Edited Edited",
+        NAME_ABBREVIATION = "MBEE",
+    }
+end
